b92b3979c3b680986c09d30fd30bb4b3371f17ec
AnneJoo <spdlqjdyd0414@naver.com>
2024-03-03 04:45:04 -0800

    react: 17.0.2 react-dom: 17.0.2 react-router-dom: 5.2.0 @material-ui/core: 4.11.3 @material-ui/icons: 4.11.2

---
+++ forum/src/components/profileFeed/ProfileFeed.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Share from "../../partials/feed/share/Share.jsx";
+
+const ProfileFeed = () => {
+    return (
+        <div className="feed">
+            <div className="feedWrapper">
+ 
+             <Share />
+
+            </div>
+        </div>
+    )
+}
+
+export default ProfileFeed;
