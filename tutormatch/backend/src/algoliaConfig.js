0327daea8c900f18f8da95072059907ce9e4fb33
atluo04 <54228854+atluo04@users.noreply.github.com>
2024-03-10 23:42:42 -0700

    Changed config files to use env variables

---
+++ tutormatch/backend/src/algoliaConfig.js
@@ -0,0 +1,25 @@
+import algoliasearch from "algoliasearch";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const algoliaConfig = {
+  posts: {
+    appId: process.env.ALGOLIA_APP_ID,
+    apiKey: process.env.ALGOLIA_POST_API_KEY,
+    indexName: "posts",
+  },
+  users: {
+    appId: process.env.ALGOLIA_APP_ID,
+    apiKey: process.env.ALGOLIA_USER_API_KEY,
+    indexName: "users",
+  },
+};
+
+const createAlgoliaClient = (indexName) => {
+  const { appId, apiKey } = algoliaConfig[indexName];
+  const algoliaClient = algoliasearch(appId, apiKey);
+  return algoliaClient.initIndex(algoliaConfig[indexName].indexName);
+};
+
+export { createAlgoliaClient };
