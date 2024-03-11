83e18419c1848b4bd7e92b7c33948ba423e65629
AnneJoo <spdlqjdyd0414@naver.com>
2024-03-11 09:26:25 -0700

    final change

--- tutormatch/frontend/src/components/chat/chatComponents/UserProfile.js
+++ tutormatch/frontend/src/components/chat/chatComponents/UserProfile.js
@@ -37,7 +37,7 @@ export default class UserProfile extends Component {
             <img src={this.state.userInfo && this.state.userInfo.image ? this.state.userInfo.image : "https://i.pinimg.com/236x/39/a1/eb/39a1eb1485516800d84981a72840d60e.jpg"} />
           </div>
           <h4>{this.state.userInfo && this.state.userInfo.name ?  this.state.userInfo.name: "Bruin"}</h4>
-          <p>{this.state.userInfo && this.state.userInfo.major ? this.state.userInfo.majors.map((major) => `*${major}\n`): "CS Student"}</p>
+          <p>{this.state.userInfo && this.state.userInfo.majors ? this.state.userInfo.majors.map((major) => `*${major}\n`): "CS Student"}</p>
         </div>
         <div className="profile__card">
           <div className="card__header" onClick={this.toggleInfo}>
